9e2043786abb7e414a1fe2ff34ac9a4259a55426
Stivan11 <104930798+Stivan11@users.noreply.github.com>
2022-05-04 21:11:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PalindromCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] characters = text.ToCharArray();
+
+            var queue = new Queue<string>(characters.Select(x => x.ToString()));
+            var stack = new Stack<string>(characters.Select(x => x.ToString()));
+
+            int count = 0;
+            int length = queue.Count();
+
+            foreach (var item in queue)
+            {
+                if (item == stack.Pop())
+                {
+                    count++;
+                }
+            }
+
+            if (count == length)
+            {
+                Console.WriteLine("Palindrom");
+            }
+        }
+    }
+}
